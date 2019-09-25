1e2fd0a1d90f0a5526b1eadaa281ef6f747ae7eb
zengkaiwei2018 <1912207369@qq,com>
2019-09-25 11:06:40 +0800

    Initial commit

---
+++ 计算正整数的和/main.swift
@@ -0,0 +1,20 @@
+
+var someInts:[Int] = [Int]()
+while true
+{
+    let input = readLine()!
+    if input != "end"
+    {
+        num = Int(input)!
+        someInts.append(num)
+    }
+    else{
+        braak;
+    }
+}
+func caculate(values:[Int])->Int
+{
+    var sum = 0
+    for item  
+}
+
